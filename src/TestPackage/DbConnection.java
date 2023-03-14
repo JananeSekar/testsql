f78f8c17cb24b56a254190dac287dafe54203392
Your Name <you@example.com>
2023-03-14 21:02:19 +0000

    second commit

---
+++ src/TestPackage/DbConnection.java
@@ -0,0 +1,39 @@
+package TestPackage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbConnection {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "root");
+
+		Statement stmt = con.createStatement();
+		
+		stmt.executeUpdate("insert into eproduct (name, price, date_added) values ('Television', 6000.00, now())");
+
+		System.out.println("The row is inserted");
+		stmt.executeUpdate("update eproduct set price=6500.00 where name='Tablet';");
+		
+		//stmt.executeUpdate("delete from eproduct where name ='Television';");
+
+		
+		ResultSet result = stmt.executeQuery("select * from eproduct");
+
+				
+		while (result.next()) {
+
+			System.out.println("Product Id " + result.getInt("ID"));
+			System.out.println("Product name " + result.getString("name"));
+		}
+		
+		con.close();
+	}
+
+}
